3ec80cd4f6c95119bee3b8c22bca449d6f2d432b
raglandconnor <raglandconnor@ufl.edu>
2024-04-08 14:56:18 -0400

    Feat: Implemented CSV Parsing (Issue #2)
    Created a struct to contain all useful data points from CSV.
    Iterates through each line of the csv adding the data points to the struct.

---
+++ src/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+using namespace std;
+
+
+struct DataNode {
+    long unsigned int ID;
+    int severity;  // 1 to 4, where 1 is low and 4 is high
+    string startTime;
+    string endTime;
+    float distance;  // Length of road affected by accident in miles
+    string streetName;
+    string city;
+    string county;
+    string state;
+    string zipcode;
+    string timezone;
+    string weatherCondition;
+    string relativeTime;  // Day or night
+};
+
+
+int main() {
+    cout << "Working directory: " << filesystem::current_path() << endl;
+    string pathname = "resources/US_Accidents_March23.csv";
+    ifstream file{pathname};
+
+    if (!file.is_open()) {
+        cout << "File not open" << endl;
+        return 1;
+    }
+
+    /*
+     * ID [1], severity [3], startTime [4], endTime [5], distance [10], streetName [12], city [13], county [14],
+     * state [15], zipcode [16], timezone [18], weatherCondition [29], relativeTime [43]
+     */
+
+    string line;
+    getline(file, line);  // Skip first line
+    while (getline(file, line)) {
+        DataNode dataObject;
+
+        stringstream ss(line);
+        int indexCount = 1;
+        string dataPoint;
+
+        while (getline(ss, dataPoint, ',')) {
+            if (indexCount == 1) {
+                dataPoint = dataPoint.substr(2, dataPoint.size()-2);
+                dataObject.ID = stoi(dataPoint);
+            }
+            else if (indexCount == 3) {
+                dataObject.severity = stoi(dataPoint);
+            }
+            else if (indexCount == 4) {
+                dataObject.startTime = dataPoint;
+            }
+            else if (indexCount == 5) {
+                dataObject.endTime = dataPoint;
+            }
+            else if (indexCount == 10) {
+                dataObject.distance = stof(dataPoint);
+            }
+            else if (indexCount == 12) {
+                dataObject.streetName = dataPoint;
+            }
+            else if (indexCount == 13) {
+                dataObject.city = dataPoint;
+            }
+            else if (indexCount == 14) {
+                dataObject.county = dataPoint;
+            }
+            else if (indexCount == 15) {
+                dataObject.state = dataPoint;
+            }
+            else if (indexCount == 16) {
+                dataObject.zipcode = dataPoint;
+            }
+            else if (indexCount == 18) {
+                dataObject.timezone = dataPoint;
+            }
+            else if (indexCount == 29) {
+                dataObject.weatherCondition = dataPoint;
+            }
+            else if (indexCount == 43) {
+                dataObject.relativeTime = dataPoint;
+            }
+
+            indexCount++;
+        }
+
+        // TODO: Insert dataObject into red-black tree and hashmap
+    }
+    return 0;
+}
