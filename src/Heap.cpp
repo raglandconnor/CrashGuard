814e2fe36e1517013bdceb06f0099014cfae4adc
Luis Cabrera <cabreraluis@ufl.edu>
2024-04-14 20:04:08 -0400

    feat: Created heap interface
    Created a draft of the heap header and wrote the definitions for the empty, size, and top functions.

--- src/Heap.cpp
+++ src/Heap.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Heap.h"
+
+void Heap::heapifyUp(){
+
+}
+
+void Heap::heapifyDown(){
+
+}
+
+Heap::Heap(){
+    _size = 0;
+
+}
+
+bool Heap::empty(){
+    return _size == 0;
+}
+
+int Heap::size(){
+    return _size;
+}
+
+DataNode Heap::top(){
+    return heap[0];
+}
+
+void Heap::pop(){
+
+}
--- src/Heap.h
+++ src/Heap.h
@@ -4,10 +4,26 @@
 
 #ifndef PROJECT3_CRASHGUARD_HEAP_H
 #define PROJECT3_CRASHGUARD_HEAP_H
+#include <vector>
+#include "DataNode.h"
+using namespace std;
+
 
 
 class Heap {
-
+    private:
+        vector<DataNode> heap;
+        int _size;
+        void heapifyUp();
+        void heapifyDown();
+    public:
+        Heap();
+        bool empty();
+        int size();
+        DataNode top();
+            // i.e, print data.numCrashes;
+            // data.whateverElse
+        void pop();
 };
 
 
