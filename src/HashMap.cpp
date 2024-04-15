acc35ede12fef832233b546972d869a4ef35b0f2
raglandconnor <raglandconnor@ufl.edu>
2024-04-15 09:21:43 -0400

    Feat: Created Interface for HashMap and Implemented updateLoadFactor
    Created a rough interface for the HashMap class using templates
    Created the initializer
    Created updateLoadFactor

--- src/HashMap.cpp
+++ src/HashMap.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "HashMap.h"
+
+
+template<typename T>
+int HashMap<T>::hash(T key) {
+
+}
+
+
+template<typename T>
+void HashMap<T>::updateLoadFactor() {
+    _loadFactor = (float)_currentCapacity / (float)_maxCapacity;
+    if (_loadFactor > _maxCapacity) {
+        int tempCapacity = _maxCapacity;  // Store old capacity
+        _maxCapacity *= 2;
+
+        vector<vector<pair<T, DataNode>>> newHashMap;
+        newHashMap.resize(_maxCapacity);
+
+        for (int i = 0; i < tempCapacity; i++) {
+            for (auto pair : hashMap[i]) {
+                // Push each pair into their respective row
+                int idx = hash(pair.first);
+                newHashMap[idx].push_back(pair);
+            }
+        }
+
+        hashMap.clear();
+        hashMap.resize(_maxCapacity);
+        hashMap = move(newHashMap);  // std::move to avoid copies
+    }
+}
+
+
+// Initializer
+template<typename T>
+HashMap<T>::HashMap() {
+    hashMap.resize(INITIAL_CAPACITY);
+    _maxCapacity = INITIAL_CAPACITY;
+    _currentCapacity = 0;
+}
+
+
+template<typename T>
+void HashMap<T>::insert(T key, DataNode dataObject) {
+
+}
+
+
+template<typename T>
+void HashMap<T>::find(T key) {
+
+}
+
+
+template<typename T>
+vector<pair<T, DataNode>> HashMap<T>::getTopK(int k) {
+
+}
+
+
+template<typename T>
+vector<pair<T, DataNode>> HashMap<T>::getBottomK(int k) {
+
+}
--- src/HashMap.h
+++ src/HashMap.h
@@ -5,9 +5,36 @@
 #ifndef PROJECT3_CRASHGUARD_HASHMAP_H
 #define PROJECT3_CRASHGUARD_HASHMAP_H
 
+#include <iostream>
+#include <vector>
+#include "DataNode.h"
 
+using namespace std;
+
+
+template <typename T>
 class HashMap {
+private:
+    const float MAX_LOAD_FACTOR = 0.75;
+    const int INITIAL_CAPACITY = 256;
 
+    float _loadFactor;  // (currentCapacity / maxCapacity)
+    int _maxCapacity;
+    int _currentCapacity;
+
+    vector<vector<pair<T, DataNode>>> hashMap;  // Stores <key, DataNode>
+
+    int hash(T key);  // Returns hash key
+    void updateLoadFactor();  // Re-hashes the hash map
+
+public:
+    HashMap();
+
+    void insert(T key, DataNode dataObject);
+    void find(T key);  // May change to accommodate specific uses
+
+    vector<pair<T, DataNode>> getTopK(int k);  // Gets top k values
+    vector<pair<T, DataNode>> getBottomK(int k);  // Gets k lowest values
 };
 
 
